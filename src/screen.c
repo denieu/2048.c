cd722271aedeb287d8829fdf1121df0990d55f20
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2021-03-25 22:22:48 -0300

    Adicionando conio2.h

--- src/screen.c
+++ src/screen.c
@@ -1,8 +1,18 @@
 #include "../includes/screen.h"
+#include "../includes/conio2.h" //gotoxy, textcolor, textbackground
 
 /*------------------------------------------------------------------------------
  * Printa a tela atual com base no "currentAppState"
  *----------------------------------------------------------------------------*/
 void printAppState(typeAppState currentAppState){
-  
+  //Printa tela menu - Raissa
+
+  //Printa tela jogo - Daniel
+
+  //Printa tela de pausa
+
+  //Printa tela instruções
+
+  //Printa tela fim de partida
+
 }
