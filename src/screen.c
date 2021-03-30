24a5eeb1e4d325d84ccf7ac6e15fffd2b9c29346
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2021-03-30 16:04:10 -0300

    Adicionando cabeçalho da func screen_inGame e nova função para esconder cursor da tela.

--- src/screen.c
+++ src/screen.c
@@ -1,13 +1,29 @@
 #include "../includes/screen.h"
+#include "windows.h" //SetConsoleCursorInfo
 #include "../includes/conio2.h" //gotoxy, textcolor, textbackground
+
+void setCursor(enum_cursorState cursorState){
+  CONSOLE_CURSOR_INFO info;
+  info.dwSize = 100;
+  info.bVisible = cursorState;
+  SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+void screen_inGame(type_appState currentAppState){
+    
+}
 
 /*------------------------------------------------------------------------------
  * Printa a tela atual com base no "currentAppState"
  *----------------------------------------------------------------------------*/
-void printAppState(typeAppState currentAppState){
+void printAppState(type_appState currentAppState){
+  //Esconde o cursos da tela
+  setCursor(CURSOR_HIDDEN);
+  
   //Printa tela menu - Raissa
 
   //Printa tela jogo - Daniel
+  //screen_inGame(currentAppState);
 
   //Printa tela de pausa
 
