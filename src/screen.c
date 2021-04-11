a885d089a0b1d089cd8c99b243a37d1d412b9a2a
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2021-04-11 11:39:55 -0300

    Reorganizando modulo screens

--- src/screen.c
+++ src/screen.c
@@ -1,7 +1,7 @@
 #include "../includes/screen.h"
+#include "../includes/screen_components.h"
 #include "../includes/logo.h"
-#include "../includes/conio2.h" //gotoxy, textcolor, textbackground
-#include "windows.h"            //SetConsoleCursorInfo
+#include "windows.h"    
 
 /*------------------------------------------------------------------------------
  * Altera o estado do cursor entre visivel e invisivel
@@ -14,72 +14,29 @@ void setCursor(enum_cursorState cursorState){
 }
 
 /*------------------------------------------------------------------------------
- * Printa na posição (X, Y) uma letra do logo, com as cores especificadas
- *----------------------------------------------------------------------------*/
-void print_logoLetter(const char letter[][9], int posX, int posY, COLORS bgColor, COLORS textColor){
-  textbackground(bgColor);
-  textcolor(textColor);
-
-  for(int count = 0; count < CHAR_LOGO_LINES; count++){
-    gotoxy(posX, posY + count);
-    printf("%s", letter[count]);
-  }
-
-  textbackground(DEFAULT_BG_COLOR);
-  textcolor(DEFAULT_TEXT_COLOR);
-}
-
-/*------------------------------------------------------------------------------
- * Printa um botão para menu, selected define se o botão está o não selecionado
- *----------------------------------------------------------------------------*/
-void print_menuButton(char * placeholder, int posX, int posY, bool selected){
-  #define buttonWidth 36
-  #define selectorWidth 3
-
-  char selection[buttonWidth] = {0};
-  int placeholderSize = strlen(placeholder);
-  int numberOfSpaces = buttonWidth - placeholderSize - selectorWidth;
-
-  for(int count = 0; count < numberOfSpaces ; count++)
-    selection[count] = ' ';
-  selection[numberOfSpaces] = '<';
-  selection[numberOfSpaces + 1] = '-';
-  selection[numberOfSpaces + 2] = '-';
-  selection[numberOfSpaces + 3] = '\0';
-
-  gotoxy(posX, posY);
-
-  if(selected == TRUE)
-    textcolor(RED);
-
-  printf("%s%s", placeholder, (selected == TRUE) ? selection : ""); 
-
-  textbackground(DEFAULT_BG_COLOR);
-  textcolor(DEFAULT_TEXT_COLOR);
-}
-
-/*------------------------------------------------------------------------------
  * Printa a tela do menu principal
  *----------------------------------------------------------------------------*/
 void screen_mainMenu(type_appState currentAppState){
-  print_logoLetter(charLogo2, 10, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
-  print_logoLetter(charLogo0, 18, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
-  print_logoLetter(charLogo4, 26, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
-  print_logoLetter(charLogo8, 34, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
-  print_logoLetter(charLogoDot, 41, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
-  print_logoLetter(charLogoC, 44, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+  int menuXStart = 10;
 
-  print_menuButton("Continuar", 12, 13, TRUE);
-  print_menuButton("Novo Jogo", 12, 15, FALSE);
-  print_menuButton("Ajuda", 12, 17, FALSE);
-  print_menuButton("Sair", 12, 19, FALSE);
+  print_logoLetter(LOGO_2, menuXStart, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+  print_logoLetter(LOGO_0, menuXStart + 8, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+  print_logoLetter(LOGO_4, menuXStart + 16, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+  print_logoLetter(LOGO_8, menuXStart + 24, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+  print_logoLetter(LOGO_DOT, menuXStart + 31, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+  print_logoLetter(LOGO_C, menuXStart + 34, 3, DEFAULT_BG_COLOR, DEFAULT_TEXT_COLOR);
+
+  print_menuButton("Continuar", menuXStart + 12, 13, TRUE);
+  print_menuButton("Novo Jogo", menuXStart + 12, 15, FALSE);
+  print_menuButton("Ajuda", menuXStart + 12, 17, FALSE);
+  print_menuButton("Sair", menuXStart + 12, 19, FALSE);
 }
 
 /*------------------------------------------------------------------------------
  * Printa a tela dentro de jogo
  *----------------------------------------------------------------------------*/
 void screen_inGame(type_appState currentAppState){
-
+  print_inGameButton("Salvar (S)", 0, 0);
 }
 
 /*------------------------------------------------------------------------------
@@ -92,12 +49,13 @@ void printAppState(type_appState currentAppState){
   //Printa tela menu - Raissa
   screen_mainMenu(currentAppState);
   getch();
-
   clrscr();
 
   //Printa tela jogo - Daniel
   screen_inGame(currentAppState);
   getch();
+  clrscr();
+
   //Printa tela de pausa
 
   //Printa tela instruções
